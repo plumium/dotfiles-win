d85cf9d385746f96bd7544ba8db5008ab1d7566d
u-u2 <u-u2@outlook.jp>
2023-07-07 09:01:20 +0900

    Change disable vi mode

--- files/powershell/profile.ps1
+++ files/powershell/profile.ps1
@@ -3,25 +3,14 @@
 Set-Alias vi 'C:\Program Files\Vim\vim90\vim.exe'
 Set-Alias eclipse 'C:\Program Files\Eclipse Foundation\2023-03-jee\eclipse\eclipse.exe'
 
+Set-PSReadLineOption `
+    -PredictionViewStyle ListView `
+
 function Prompt {
     Write-Host "${env:USERNAME}@${env:COMPUTERNAME} " -NoNewLine -ForegroundColor "Cyan"
     Write-Host $pwd -NoNewLine -ForegroundColor "DarkCyan"
     return ">"
 }
-function OnViModeChange {
-    if ($args[0] -eq 'Command') {
-        # Set the cursor to a blinking block.
-        Write-Host -NoNewLine "`e[1 q"
-    }
-    else {
-        # Set the cursor to a blinking line.
-        Write-Host -NoNewLine "`e[5 q"
-    }
-}
-Set-PSReadLineOption `
-    -PredictionViewStyle ListView `
-    -EditMode Vi `
-    -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
 
 # function
 function Which($name) {
