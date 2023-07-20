cf60950cc73d1ae198c6a2f1054ea57101588570
u-u2 <u-u2@outlook.jp>
2023-07-20 10:51:57 +0900

    Remove scoop and dbcli

--- install.ps1
+++ install.ps1
@@ -1,27 +1,6 @@
 $caller = pwd
 cd "$PSScriptRoot\files"
 
-# install scoop
-try {
-    if (Get-Command scoop) {
-    }
-}
-catch {
-    irm get.scoop.sh | iex
-}
-try {
-    if (Get-Command zenhan) {
-    }
-}
-catch {
-    scoop install zenhan
-}
-
-# dbcli pgcli
-cp -Force `
-    -Path .\dbcli\pgcli\config `
-    -Destination "$env:LOCALAPPDATA\dbcli\pgcli\config"
-
 Start-Process -Verb runas -Wait pwsh.exe( `
         "-Command cd $pwd;" + `
         "$PSScriptRoot\scripts\runas.ps1;")
