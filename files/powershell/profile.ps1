e873fab80d0ef775ad2a3f250926cbb0dea7ccde
u-u2 <u-u2@outlook.jp>
2023-09-27 13:04:35 +0900

    add completion of winget

--- files/powershell/profile.ps1
+++ files/powershell/profile.ps1
@@ -45,3 +45,18 @@ function Get-CurrentGitBranchName ([string]$Path = ".\") {
         ForEach-Object { $_.Matches.Value }
 }
 
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param(
+            $wordToComplete,
+            $commandAst,
+            $cursorPosition
+         )
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition |
+        ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+
