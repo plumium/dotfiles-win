0317ec646ebd3578286d8de4e70de3a343f7b83b
plumium <plumium@outlook.com>
2024-07-25 20:21:23 +0900

    fix syntax error on WindowsPowerShell 5.1

--- scripts/symlink.ps1
+++ scripts/symlink.ps1
@@ -17,32 +17,32 @@ function ProcessBegin {
 }
 
 function ProcessFile {
-  $input
-    | ForEach-Object {
-      $path = $_.FullName
-        | ForEach-Object Replace $targetRoot ''
-        | ForEach-Object Substring(1)
-        | ForEach-Object { Join-Path $env:USERPROFILE $_ }
+  $input |
+    ForEach-Object {
+      $path = $_.FullName |
+        ForEach-Object Replace $targetRoot '' |
+        ForEach-Object Substring(1) |
+        ForEach-Object { Join-Path $env:USERPROFILE $_ }
 
-      Write-Output "$path -> $($_.FullName)"
-      if (-not $dryRun) {
-        New-Item -Force -ItemType SymbolicLink -Path $path -Target $_.FullName
-          | Out-Null
-      }
+        Write-Output "$path -> $($_.FullName)"
+        if (-not $dryRun) {
+          New-Item -Force -ItemType SymbolicLink -Path $path -Target $_.FullName |
+            Out-Null
+        }
     }
 }
 
 function ProcessDirectory {
-  $input
-    | Get-ChildItem -Recurse -File
-    | ForEach-Object {
-      $_.Directory
-        | ForEach-Object FullName
-        | ForEach-Object Replace $targetRoot ''
-        | ForEach-Object Substring(1)
-        | ForEach-Object { Join-Path $env:USERPROFILE $_ }
-        | Where-Object { -not(Test-Path $_) }
-        | ForEach-Object { mkdir $_ | Out-Null }
+  $input |
+    Get-ChildItem -Recurse -File |
+      ForEach-Object {
+        $_.Directory |
+          ForEach-Object FullName |
+          ForEach-Object Replace $targetRoot '' |
+          ForEach-Object Substring(1) |
+          ForEach-Object { Join-Path $env:USERPROFILE $_ } |
+          Where-Object { -not(Test-Path $_) } |
+          ForEach-Object { mkdir $_ | Out-Null }
 
       $_ | ProcessFile
     }
@@ -57,8 +57,8 @@ foreach ($item in @(
       )) {
   Write-Output "$($item[0]) -> $($item[1])"
   if (-not $dryRun) {
-    New-Item -Force -ItemType SymbolicLink -Path $item[0] -Target $((Get-ChildItem $item[1]).FullName)
-      | Out-Null
+    New-Item -Force -ItemType SymbolicLink -Path $item[0] -Target $((Get-ChildItem $item[1]).FullName) |
+      Out-Null
   }
 }
 
